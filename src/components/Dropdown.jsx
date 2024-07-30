82470f250c6a60f8a63a81c774e6e7ed6a79bee0
jlee927 <jlee927@uic.edu>
2024-07-30 09:17:55 -0500

    Created navbar

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,25 @@
+import "./navbar.css";
+import React from "react";
+
+export default function Dropdown(props) {
+   const linkNames = props.linkNames || [];
+
+   let links = linkNames.map((link, index) => {
+      return (
+         <a key={index} href="#">
+            {link}
+         </a>
+      );
+   });
+   return (
+      <div>
+         <div className="dropdown">
+            <button>
+               {props.name} <i className="fa fa-caret-down"></i>
+            </button>
+
+            <div className="dropdown-content">{links}</div>
+         </div>
+      </div>
+   );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import "./navbar.css";
+import Dropdown from "./Dropdown";
+import logo from "../images/logoSP.png";
+
+export default function Navbar() {
+   return (
+      <div className="navbar">
+         <div className="navbar--leftSide">
+            <img className="navbar--image" src={logo} />
+            <h2 className="navbar--mainText">생명샘 장로교회</h2>
+         </div>
+
+         <div className="navbar--rightSide">
+            <a>홈</a>
+            <Dropdown name="교회 안내" linkNames={["교회 소식", "목회 철학"]} />
+            <Dropdown
+               name="설교"
+               linkNames={["주일 설교", "기타 설교", "에베소서 강해"]}
+            />
+            <a>매일성경</a>
+            <a>박화신 목사 컬럼</a>
+            <Dropdown
+               name="갤러리"
+               linkNames={["동영상들", "최근 사진들", "보관된 오랜 사진들"]}
+            />
+         </div>
+      </div>
+   );
+}
