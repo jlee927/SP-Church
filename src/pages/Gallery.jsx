9b76b49c6522ef392b882bed89060ce2ec570d98
jlee927 <jlee927@uic.edu>
2024-07-31 15:32:00 -0500

    deleted the old youtube css

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,12 +1,7 @@
-import "../assets/styles/youtube.css";
-import Navbar from "../components/Navbar";
-import YoutubeEmbed from "../components/Youtube";
-
 export default function Gallery() {
    return (
       <div>
-         {/* <h1>Test</h1> */}
-        <YoutubeEmbed embedId="K4TOrB7at0Y"/>
+         <h1> My Test Gallery</h1>
       </div>
    );
 }
